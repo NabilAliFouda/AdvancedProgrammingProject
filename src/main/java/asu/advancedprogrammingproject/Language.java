af11faaa5d5c80bcef5f27e8a0363e079c97e0f3
nabil <nabil@192.168.1.16>
2025-05-16 01:17:55 +0300

    added language and courses classes

--- src/main/java/asu/advancedprogrammingproject/Language.java
+++ src/main/java/asu/advancedprogrammingproject/Language.java
@@ -9,5 +9,27 @@ package asu.advancedprogrammingproject;
  * @author nabil
  */
 public class Language {
-    
+    private String LanguageName;
+    private int StudentNo;
+    private Teacher teachers[];
+    private Course courses[];
+    public Language(int StudentNo, Teacher teachers[], Course courses[]){
+        this.StudentNo = StudentNo;
+        this.teachers = teachers;
+        this.courses = courses;
+    }
+    public Language(){
+        this.StudentNo = 0;
+        this.teachers = new Teacher[0];
+        this.courses = new Course[0];
+    }
+    public Teacher[] getteachers() {
+        return teachers;
+    }
+    public Course[] getcourses() {
+        return courses;
+    }  
+    public String getLanguageName() {
+        return LanguageName;
+    }
 }
