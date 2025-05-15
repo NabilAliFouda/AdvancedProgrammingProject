b5b8cd6e4435dd6b63ad3aa39e40bd90da23b28d
Kareem Saleh <kareim3200@gmail.com>
2025-05-16 01:29:58 +0300

    Update User class

--- src/main/java/asu/advancedprogrammingproject/Student.java
+++ src/main/java/asu/advancedprogrammingproject/Student.java
@@ -11,3 +11,4 @@ package asu.advancedprogrammingproject;
 public class Student {
     
 }
+
--- src/main/java/asu/advancedprogrammingproject/User.java
+++ src/main/java/asu/advancedprogrammingproject/User.java
@@ -8,6 +8,49 @@ package asu.advancedprogrammingproject;
  *
  * @author nabil
  */
-public class User {
-    
+public abstract class User {
+    protected String name;
+    protected int ID;
+    protected String password;
+    protected boolean loggedIn;
+
+    User(){
+        this.name = " ";
+        this.ID = 0;
+        this.password = " ";
+        this.loggedIn = false;     
+    }
+
+    User(String name , int ID , String password) {
+        this.name = name ; 
+        this.ID = ID;
+        this.password = password;
+        this.loggedIn = false;
+    }
+
+    public void logIn (int ID , String password) {
+        if (loggedIn) {
+            System.out.println("User already logged in.");
+            return;
+        }
+        if(this.password.equals(password) && this.ID == ID){
+            System.out.println("Logged in successfully.");
+            loggedIn = true;
+        } 
+        else{
+            System.out.println("Incorrect ID or password.");
+        } 
+    }
+    public void logout(){
+        if(loggedIn){
+            loggedIn = false;
+            System.out.println("Logged out successfully.");
+        }
+        else {
+            System.out.println("User is already logged out");
+        }
+    }
+
+    public abstract void getRole();
+       
 }
